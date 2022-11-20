da162b471884a0a8f55f097a0b3eca35be62619d
bors <bors@users.noreply.github.com>
2022-11-20 20:20:12 +0000

    deploy: 26562973b3482a635416b2b663a13016d4d90e20

---
+++ implementors/lsp_types/notification/trait.Notification.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"rust_analyzer":[["impl Notification for <a class=\"enum\" href=\"rust_analyzer/lsp_ext/enum.ServerStatusNotification.html\" title=\"enum rust_analyzer::lsp_ext::ServerStatusNotification\">ServerStatusNotification</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
